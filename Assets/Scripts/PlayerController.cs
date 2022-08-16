683a50b7a964a24bd1be92be9b0855ca01abd863
Alexander Karpov <kukuruku@yandex-team.ru>
2022-08-16 16:47:08 +0300

    Добавляет прыжки

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 
 namespace Gum
 {
@@ -9,7 +13,8 @@ namespace Gum
     {
         CharacterController controller;
 
-        PlayerInput playerInput;
+        [SerializeField]
+        InputActionReference TouchAction;
 
         [Header("Movement")]
         [SerializeField]
@@ -22,43 +27,96 @@ namespace Gum
 
         float rotationVelocity;
 
-        [Header("Jump and gravity")]
-        [SerializeField]
-        float gravity = 9.8f;
+        int _movementTouchId = -1;
 
+        Vector2 _movementTouchStartPoint;
+
+        Vector2 _movementTouchCurrentPoint;
+
+        [Header("Jump and gravity")]
         [SerializeField]
         float gravityMultiplier = 2;
 
         [SerializeField]
-        float groundedVelocity = -0.5f;
-
-        [SerializeField]
         float jumpHeight = 3f;
 
-        float yVelocity;
+        float _yVelocity;
 
-        bool jumpPrepared;
+        bool _jumpPrepared;
 
-        // Start is called before the first frame update
         void Start()
         {
             controller = GetComponent<CharacterController>();
-            playerInput = GetComponent<PlayerInput>();
+        }
+
+        void OnEnable()
+        {
+            TouchAction.action.Enable();
+            TouchAction.action.performed += TouchPerformed;
+        }
+
+        void OnDisable()
+        {
+            TouchAction.action.performed -= TouchPerformed;
+        }
+
+        private void TouchPerformed(InputAction.CallbackContext obj)
+        {
+            var touch = obj.ReadValue<TouchState>();
+
+            if (touch.isTap)
+            {
+                // Прыжок тапом по плавой части экрана
+                _jumpPrepared = touch.isTap && touch.position.x > Screen.width / 2;
+                Debug.Log (_jumpPrepared);
+                Debug.Log(touch.phase);
+            }
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                var isMovementTouch = touch.position.x < Screen.width / 2;
+
+                if (isMovementTouch)
+                {
+                    _movementTouchId = touch.touchId;
+                    _movementTouchStartPoint = touch.position;
+                    _movementTouchCurrentPoint = touch.position;
+                }
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (touch.touchId == _movementTouchId)
+                {
+                    _movementTouchCurrentPoint = touch.position;
+                }
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                if (touch.touchId == _movementTouchId)
+                {
+                    _movementTouchId = -1;
+                    _movementTouchStartPoint = touch.position;
+                    _movementTouchCurrentPoint = touch.position;
+                }
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
-            HandleGravityAndJump();
+            var jumpReplacement = HandleGravityAndJump();
 
-            var move = playerInput.actions["Move"].ReadValue<Vector2>();
+            var move = _movementTouchCurrentPoint - _movementTouchStartPoint;
 
             if (move == Vector2.zero)
             {
+                controller.Move (jumpReplacement);
                 return;
             }
 
-            Vector3 input = new Vector3(move.x, 0, move.y);
+            var input = new Vector3(move.x, 0, move.y).normalized;
 
             float directionAngle =
                 Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
@@ -70,47 +128,30 @@ namespace Gum
                     ref rotationVelocity,
                     rotationSmoothTime);
 
-            Vector3 direction = Quaternion.Euler(0, directionAngle, 0) * Vector3.forward;
+            var direction = Quaternion.Euler(0, directionAngle, 0) * Vector3.forward;
 
             transform.rotation = Quaternion.Euler(0, currentAngle, 0);
-            controller.Move(speed * Time.deltaTime * direction);
+            controller.Move(speed * Time.deltaTime * direction + jumpReplacement);
         }
 
-        void HandleGravityAndJump()
+        Vector3 HandleGravityAndJump()
         {
-            var jump = playerInput.actions["Jump"];
-            var look = playerInput.actions["Look"];
-
-            if (controller.isGrounded && yVelocity < 0f)
+            if (controller.isGrounded)
             {
-                yVelocity = groundedVelocity;
+                if (_jumpPrepared)
+                {
+                    _yVelocity = Mathf.Sqrt(jumpHeight * 2f * -Physics.gravity.y);
+                }
+                else
+                {
+                    _yVelocity = 0;
+                }
             }
 
-            if (!jumpPrepared && jump.WasPressedThisFrame())
-            {
-                jumpPrepared = true;
-            }
+            _yVelocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+            _jumpPrepared = false;
 
-            if (look.triggered)
-            {
-                jumpPrepared = false;
-            }
-
-            if (
-                controller.isGrounded &&
-                jumpPrepared &&
-                playerInput.actions["Jump"].WasReleasedThisFrame()
-            )
-            {
-                yVelocity = Mathf.Sqrt(jumpHeight * 2f * gravity);
-                jumpPrepared = false;
-
-                InputSystem.QueueDeltaStateEvent(Gamepad.current.rightStick, new Vector2(0.5f, 0f));
-                Debug.Log("QueueDeltaStateEvent");
-            }
-
-            yVelocity -= gravity * gravityMultiplier * Time.deltaTime;
-            controller.Move(yVelocity * Time.deltaTime * Vector3.up);
+            return _yVelocity * Time.deltaTime * Vector3.up;
         }
     }
 }
